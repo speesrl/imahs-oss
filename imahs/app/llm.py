f35933bbbdff0241a5cfa8dceb85bec1d24ee878
alyshmahell <aly.shmahell@gmail.com>
2025-09-19 21:20:12 +0200

    debugging

--- imahs/app/llm.py
+++ imahs/app/llm.py
@@ -6,11 +6,15 @@ from lib.clients import ReRanker
 
 @ui.page("/")
 def index():
-    reranker = ReRanker(
-        url=os.environ.get("url_infinity"), model="cross-encoder/mmarco-mMiniLMv2-L12-H384-v1"
-    )
+    try:
+        reranker = ReRanker(
+            url=os.environ.get("url_infinity"), model="cross-encoder/mmarco-mMiniLMv2-L12-H384-v1"
+        )
 
-    logging.info(
-        reranker("hello there mr.", ["greetings", "salutations", "camels"])
-    )
+        logging.info(
+            reranker("hello there mr.", ["greetings", "salutations", "camels"])
+        )
+    except Exception as e:
+        logging.exception(e)
+        logging.exception(os.environ.get("url_infinity"))
 ui.run(host='0.0.0.0', port=8118)
